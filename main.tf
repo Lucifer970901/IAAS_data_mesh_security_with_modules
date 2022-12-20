c1c1796b88bf3c15a1c3dff1a758a87c56b91d00
Meghashree N <meghashreetendulkar@gmail.com>
2022-12-20 21:41:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 module "main_compartment" {
   source              = "./compartments"
   compartment_id      = var.Parent_compartment_id
-  name                = var.compartment_name
+  compartment_name                = var.compartment_name
   
 }
   
