59fff15314d25b683df97296c6e7aed84e171618
Meghashree N <meghashreetendulkar@gmail.com>
2022-12-20 20:48:32 +0530

    Create outputs.tf

---
+++ compartments/outputs.tf
@@ -0,0 +1,10 @@
+output "compartments" {
+  description = "The VCNs, indexed by display_name."
+  value       = oci_identity_compartment.Retail_compartment.display_name
+}
+
+output "compartment_id_output" {
+  description = "The main compartment id."
+  value = oci_identity_compartment.Retail_compartment.id
+}
+
