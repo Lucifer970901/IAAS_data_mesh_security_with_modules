9a7fe0062d21f5f52a0fd8802412158e6b773c67
Meghashree N <meghashreetendulkar@gmail.com>
2022-12-20 21:28:19 +0530

    Update compartment.tf

--- compartments/compartment.tf
+++ compartments/compartment.tf
@@ -1,7 +1,6 @@
 #VCN resource block
 resource "oci_identity_compartment" "Retail_compartment" {
-    compartment_id = var.Parent_compartment_id
-    description = var.compartment_description
-    display_name = var.compartment_name
+    compartment_id = "{$var.Parent_compartment_id}"
+    name = var.compartment_name
     
 }
