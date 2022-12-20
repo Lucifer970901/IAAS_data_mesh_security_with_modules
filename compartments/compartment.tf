4e4d427e3f3b468ff9828d97b3f1a4ff72d89d44
Meghashree N <meghashreetendulkar@gmail.com>
2022-12-20 21:54:16 +0530

    Update compartment.tf

--- compartments/compartment.tf
+++ compartments/compartment.tf
@@ -2,5 +2,6 @@
 resource "oci_identity_compartment" "Retail_compartment" {
     compartment_id = "{$var.Parent_compartment_id}"
     name = var.compartment_name
+    description = var.compartment_description
     
 }
