7991589127abfde707725a0ac01b641130ae01d0
Meghashree N <meghashreetendulkar@gmail.com>
2022-12-20 21:59:25 +0530

    Update outputs.tf

--- compartments/outputs.tf
+++ compartments/outputs.tf
@@ -1,6 +1,6 @@
 output "compartments" {
   description = "The VCNs, indexed by display_name."
-  value       = oci_identity_compartment.Retail_compartment.display_name
+  value       = oci_identity_compartment.Retail_compartment.name
 }
 
 output "compartment_id_output" {
