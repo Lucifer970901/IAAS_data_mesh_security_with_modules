77806ef270e91e32cc50505c7a97a6a9190219cd
Meghashree N <meghashreetendulkar@gmail.com>
2022-12-20 20:38:00 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,7 @@
+module "main_compartment" {
+  source              = "./compartment"
+  compartment_id = var.Parent_compartment_id
+  compartment_name = var.compartment_name
+  description = var.description
+}
+  
