9c4543fe93fb6ec96bd74460bafcb26e7b8eaa04
Meghashree N <meghashreetendulkar@gmail.com>
2022-12-21 16:51:59 +0530

    Update variables.tf

--- compartments/variables.tf
+++ compartments/variables.tf
@@ -16,3 +16,14 @@ variable "compartment_name" {
 }
 
 
+#parameters of sub compartments
+variable "compartments" {
+   type        = any
+   default     = {}
+}
+
+variable "main_compartment_id" {
+  description = " compartment id under which this compartment is created"
+  type        = string
+
+}
