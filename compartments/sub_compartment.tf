beb45953658e6296a2980aa7809483c05ef7f562
Meghashree N <meghashreetendulkar@gmail.com>
2022-12-21 16:53:03 +0530

    Create sub_compartment.tf

---
+++ compartments/sub_compartment.tf
@@ -0,0 +1,11 @@
+resource "oci_identity_compartment" "sub_compartments" {
+    #Required
+      
+     compartment_id = "${var.compartment_id}"
+    
+    for_each       = var.subcompartments   
+    name    = lookup(each.value, "name", each.key)
+    description = each.value.description
+    enable_delete = each.value.enable_delete
+    
+}
