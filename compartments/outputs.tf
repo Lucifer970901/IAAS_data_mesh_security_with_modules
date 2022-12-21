39fcd14b74af6786c9404d76f7dbcd08abe8ac0e
Meghashree N <meghashreetendulkar@gmail.com>
2022-12-21 17:20:21 +0530

    Update outputs.tf

--- compartments/outputs.tf
+++ compartments/outputs.tf
@@ -7,7 +7,4 @@ output "compartment_id_output" {
   description = "The main compartment id."
   value = oci_identity_compartment.Retail_compartment.id
 }
-output "sub_compartment_id" {
-  value = { for v in oci_identity_compartment.sub_compartments : v.display_name => v.id }
-}
 
